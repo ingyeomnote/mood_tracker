a9b6dd4f1c605ad091d4b23243af94770d5e768a
ingyeomnote <freeartjin@naver.com>
2024-04-30 16:33:04 +0900

    주석 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:app_test_01/mood_tracker_home_page.dart';
 
-// 애플리케이션의 진입점입니다.
+// 애플리케이션의 진입점이다.
 void main() => runApp(MoodTrackerApp());
 
-// 애플리케이션의 최상위 위젯을 정의하는 클래스입니다.
-// StatelessWidget을 상속받아, 상태가 없는 위젯임을 나타냅니다.
+// 애플리케이션의 최상위 위젯을 정의하는 클래스이다.
+// StatelessWidget을 상속받아, 상태가 없는 위젯임을 나타낸다.
 class MoodTrackerApp extends StatelessWidget {
   // flutter에서 모든 위젯은 선택적으로 'key'매개변수를 받을 수 있어야 한다.
   // 'Key? key'는 key가 null일 수도 있다는 null safety 기능
@@ -13,13 +13,13 @@ class MoodTrackerApp extends StatelessWidget {
   MoodTrackerApp({Key? key}) : super(key : key);
   @override
   Widget build(BuildContext context) {
-    // MaterialApp은 Material Design 앱을 생성하기 위한 루트 위젯입니다.
+    // MaterialApp은 Material Design 앱을 생성하기 위한 루트 위젯이다.
     return MaterialApp(
-      title: 'Mood Tracker', // 앱의 타이틀을 정의합니다.
+      title: 'Mood Tracker', // 앱의 타이틀을 정의한다.
       theme: ThemeData(
-        primarySwatch: Colors.green, // 앱의 기본 색상 테마를 설정합니다.
+        primarySwatch: Colors.green, // 앱의 기본 색상 테마를 설정한다.
       ),
-      home: MoodTrackerHomePage(), // 앱이 시작될 때 표시될 홈페이지를 지정합니다.
+      home: MoodTrackerHomePage(), // 앱이 시작될 때 표시될 홈페이지를 지정한다.
     );
   }
 }
--- lib/mood_manager.dart
+++ lib/mood_manager.dart
@@ -2,17 +2,17 @@ import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
 /*
-  MoodManager는 기분과 관련된 데이터를 저장하고 로드하는 클래스입니다.
-  이 클래스는 SharedPreferences를 사용하여 로컬 스토리지에 데이터를 저장합니다.
+  MoodManager는 기분과 관련된 데이터를 저장하고 로드하는 클래스이다.
+  이 클래스는 SharedPreferences를 사용하여 로컬 스토리지에 데이터를 저장한다.
 */
 class MoodManager {
   /*
-    저장된 기분 데이터를 로드합니다.
+    저장된 기분 데이터를 로드한다.
 
-    이 함수는 SharedPreferences에서 'moods' 키를 사용하여 저장된 데이터를 불러옵니다.
-    데이터는 JSON 형식으로 저장되며, 로드 시 Map<DateTime, List<dynamic>>으로 변환됩니다.
+    이 함수는 SharedPreferences에서 'moods' 키를 사용하여 저장된 데이터를 불러온다.
+    데이터는 JSON 형식으로 저장되며, 로드 시 Map<DateTime, List<dynamic>>으로 변환된다.
 
-    반환값: - Future<Map<DateTime, List<dynamic>>>: 비동기적으로 날짜와 관련된 기분 데이터를 담은 Map을 반환합니다.
+    반환값: - Future<Map<DateTime, List<dynamic>>>: 비동기적으로 날짜와 관련된 기분 데이터를 담은 Map을 반환한다.
   */
   Future<Map<DateTime, List<dynamic>>> loadMoods() async {
     final prefs = await SharedPreferences.getInstance();
@@ -24,16 +24,16 @@ class MoodManager {
   }
 
   /*
-    기분 데이터를 저장합니다.
+    기분 데이터를 저장한다.
    
-    이 함수는 주어진 기분 데이터를 JSON 형식으로 인코딩하고, SharedPreferences에 저장합니다.
-    각 기분 데이터는 특정 날짜에 대응되며, 이 날짜는 ISO8601 문자열 형식으로 키로 사용됩니다.
+    이 함수는 주어진 기분 데이터를 JSON 형식으로 인코딩하고, SharedPreferences에 저장한다.
+    각 기분 데이터는 특정 날짜에 대응되며, 이 날짜는 ISO8601 문자열 형식으로 키로 사용된다.
    
     매개변수:
-      - moods: 날짜별로 기분과 메모를 포함하는 데이터를 담은 Map입니다.
+      - moods: 날짜별로 기분과 메모를 포함하는 데이터를 담은 Map이다.
    
     반환값:
-      - Future<void>: 데이터 저장 작업이 완료되었음을 나타내는 Future입니다.
+      - Future<void>: 데이터 저장 작업이 완료되었음을 나타내는 Future이다.
   */
   Future<void> saveMoods(Map<DateTime, List<dynamic>> moods) async {
     final prefs = await SharedPreferences.getInstance();
