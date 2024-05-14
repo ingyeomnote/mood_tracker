f102c1d5d21418b47e8e95399c60a4aa8d44ce81
ingyeomnote <freeartjin@naver.com>
2024-05-14 16:00:42 +0900

    오타 수정

--- lib/mood_tracker_home_page.dart
+++ lib/mood_tracker_home_page.dart
@@ -126,7 +126,7 @@ class _MoodTrackerHomePageState extends State<MoodTrackerHomePage> {
              // print("_momoController $_memoController");
             //_saveMood(); // 변경된 기분을 저장..이걸 save 버튼 눌렀을 때로..
           },
-          onSavePressd: (){
+          onSavePressed: (){
             _saveMood();
           },
         );
